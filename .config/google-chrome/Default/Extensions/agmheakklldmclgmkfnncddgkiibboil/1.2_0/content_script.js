a72e390cf89c45bc736a813df573b96b952ebd33
Kevin Bock <kevin.bock@gateprotect.com>
2014-09-05 14:18:57 +0200

    remove .config dir

--- .config/google-chrome/Default/Extensions/agmheakklldmclgmkfnncddgkiibboil/1.2_0/content_script.js
+++
@@ -1,6 +0,0 @@
-var s=document.createElement("script");
-s.type="text/javascript";
-s.src="https://my.plexapp.com/queue/bookmarklet_payload";
-var h=document.getElementsByTagName("head")[0];
-h.appendChild(s);
-void(0);
--- .config/google-chrome/Default/Extensions/dhdgffkkebhmkfjojejmpbldmpobfkfo/3.8.52_0/cm/keymap/windows.js
+++
@@ -1,1 +0,0 @@
-(function(){CodeMirror.keyMap.windows={"Ctrl-Q":"close",F3:"findNext",F4:"findPrev","Ctrl-R":"replace","Ctrl-J":"jump",ESC:"close",fallthrough:["default"]}})();
